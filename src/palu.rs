8f138ded891f3b30812ff7f99e3b2038d83d058d
Mubelotix <mubelotix@gmail.com>
2023-10-19 22:15:48 +0200

    Add permutations

--- src/palu.rs
+++ src/palu.rs
@@ -23,6 +23,45 @@ pub fn find_u(a: &Matrix) -> Matrix {
     u
 }
 
+/// Finds U such as `PA=LU`
+pub fn find_pu(a: &Matrix) -> (Vec<usize>, Matrix) {
+    assert_eq!(a.n, a.p, "a isn't square");
+    let (n, p) = (a.n, a.p);
+
+    let mut pivots = Vec::new();
+    let mut u = a.to_owned();
+    for k in 0..p-1 {
+        let mut pivot = k;
+        for z in k+1..n {
+            if u[(z, k)] > u[(k, k)] {
+                pivot = z;
+            }
+        }
+        pivots.push(pivot);
+        println!("pivot between L{} and L{}", k+1, pivot+1);
+
+        let inter = u.line(k);
+        u.set_line(k, u.line(pivot));
+        u.set_line(pivot, inter);
+
+        let mut tau_k = (1.0 / u[(k,k)]) * u.column(k);
+        for j2 in 0..=k {
+            tau_k[j2] = 0.0;
+        }
+
+        let mut e_k = Matrix::new_column(n);
+        e_k[k] = 1.0;
+
+        let m_j = Matrix::id(n) - tau_k * e_k.t();
+
+        u = m_j * u;
+
+        println!("{u:?}")
+    }
+
+    (pivots, u)
+}
+
 // broken
 pub fn lu_fact(a: &Matrix) -> (Matrix, Matrix) {
     assert_eq!(a.n, a.p, "a isn't square");
@@ -63,9 +102,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_lu_fact() {
+    fn test_u() {
         let a = Matrix::from(vec![vec![-1, -1, -1], vec![-1, 1, -1], vec![1, 2, 2]]);
         let u = find_u(&a);
+        println!("{u:?}")
+    }
+
+    #[test]
+    fn test_pu() {
+        let a = Matrix::from(vec![vec![3, 17, 10], vec![2, 4, -2], vec![6, 18, -12]]);
+        let u = find_pu(&a);
         println!("{u:?}")
     }
 }
